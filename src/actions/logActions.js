34ee4ea86de3a1d20cc2556f28cd7bc165e48fa4
Bogdan Negura <id.bogdann@yahoo.com>
2019-10-29 20:49:31 +0000

    fix indentation and spacing

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,13 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from './types'
+import { 
+    GET_LOGS, 
+    SET_LOADING, 
+    LOGS_ERROR, 
+    ADD_LOG, 
+    DELETE_LOG,
+    UPDATE_LOG,
+    SET_CURRENT,
+    CLEAR_CURRENT
+ } from './types'
 
 // export const getLogs = () => {
 //     return async dispatch => {
@@ -19,21 +28,19 @@ export const getLogs = () => async dispatch => {
         try {
             setLoading()
 
-        const res = await fetch('/logs')
-        const data = await res.json()
+            const res = await fetch('/logs')
+            const data = await res.json()
 
-        dispatch({
-            type: GET_LOGS,
-            payload: data
-        })
+            dispatch({
+                type: GET_LOGS,
+                payload: data
+            })
         } catch (err) {
             dispatch({
                 type:LOGS_ERROR,
                 payload: err.response.data
             })
         }
-        
-
 }
 
 // Add new log
@@ -41,49 +48,86 @@ export const addLog = (log) => async dispatch => {
     try {
         setLoading()
 
-    const res = await fetch('/logs', {
-        method: 'POST',
-        body: JSON.stringify(log),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    const data = await res.json()
+        const res = await fetch('/logs', {
+            method: 'POST',
+            body: JSON.stringify(log),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await res.json()
 
-    dispatch({
-        type: ADD_LOG,
-        payload: data
-    })
+        dispatch({
+            type: ADD_LOG,
+            payload: data
+        })
     } catch (err) {
         dispatch({
             type:LOGS_ERROR,
             payload: err.response.data
         })
     }
-    
-
 }
 // Delete log from server
 export const deleteLog = (id) => async dispatch => {
     try {
         setLoading()
 
-    const res = await fetch(`/logs/${id}`, {
-        method: 'DELETE'
-    })
+        const res = await fetch(`/logs/${id}`, {
+            method: 'DELETE'
+        })
 
-    dispatch({
-        type: DELETE_LOG,
-        payload: id
-    })
+        dispatch({
+            type: DELETE_LOG,
+            payload: id
+        })
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        })
+    }
+}
+
+// Update log on server
+export const updateLog = log => async dispatch => {
+    try {
+        setLoading()
+
+        await fetch(`/logs/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(log),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        dispatch({
+            type: UPDATE_LOG,
+            payload: data
+        })
     } catch (err) {
         dispatch({
             type:LOGS_ERROR,
             payload: err.response.data
         })
     }
-    
+}
 
+// Set curent log
+export const setCurrent = log => {
+    return {
+        type: SET_CURRENT,
+        payload: log
+    }
+}
+
+// Clear curent log
+export const clearCurrent = log => {
+    return {
+        type: CLEAR_CURRENT,
+        payload: log
+    }
 }
 
 // Set loading to true
@@ -91,5 +135,4 @@ export const setLoading = () => {
     return {
         type: SET_LOADING
     }
-    
 }
