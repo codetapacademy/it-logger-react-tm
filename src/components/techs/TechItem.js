335fc5ed10af2cf85046726deb07d3f1b17bfa0c
Bogdan Negura <id.bogdann@yahoo.com>
2019-11-03 22:14:01 +0000

    i got it now

--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import PropTypes from 'prop-types'
+import {object, func} from 'prop-types'
 import { deleteTech } from '../../actions/techActions'
 import M from 'materialize-css/dist/js/materialize.min.js'
 
@@ -24,7 +24,7 @@ const TechItem = ({ tech: { id, firstName, lastName } , deleteTech }) => {
     )
 }
 
-const {object, func} = PropTypes
+// const {object, func} = PropTypes
 
 TechItem.propTypes = {
     tech: object.isRequired,
